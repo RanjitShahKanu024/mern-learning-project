29950042a26274f877f7b90dc8c768e447a6dd12
Ranjit Shah Kanu <ranjitshahkanu.2024cse@sece.ac.in>
2025-12-01 15:17:20 +0530

    componenets/Navbar.js and pages/App.js basic coding was done for creating table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>Hello World</h1>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Home />
     </div>
   );
 }
